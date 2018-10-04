1158b17697faba783da5a1e6b25b51dac58f9f8c
Akash Sen <43550905+picpanda@users.noreply.github.com>
2018-10-04 22:42:12 +0530

    Update: Added link to question
    Better interface and easy access to questions.

--- Introduction/ReadingRawInput.py
+++ Introduction/ReadingRawInput.py
@@ -4,6 +4,7 @@ Subdomain : Introduction
 Domain    : Python
 Author    : Ahmedur Rahman Shovon
 Created   : 15 July 2016
+Question Link: https://www.hackerrank.com/challenges/py-if-else/problem
 '''
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 name=raw_input()
