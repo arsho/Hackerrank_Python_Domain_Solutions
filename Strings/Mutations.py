373a7fbc8b0b9ac8bc5e0ce13c6c6851c3d6a2d7
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2019-10-25 09:37:00 +0600

    updated mutation problem solution as it is updated in Hackerrank

--- Strings/Mutations.py
+++ Strings/Mutations.py
@@ -4,12 +4,9 @@ Subdomain : Strings
 Domain    : Python
 Author    : Ahmedur Rahman Shovon
 Created   : 15 July 2016
+Updated   : 25 October 2019
 Problem   : https://www.hackerrank.com/challenges/python-mutations/problem
 '''
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-s=raw_input()
-in_str_ar=raw_input().strip().split()
-pos=int(in_str_ar[0])
-c=in_str_ar[1]
-final_str=s[:pos]+c+s[pos+1:]
-print final_str
+def mutate_string(string, position, character):
+    return string[:position]+character+string[position+1:]
+
