5663dc39d936ff7ff35c2bb459572b765b30f0e3
Divya Kelaskar <56636487+divyakelaskar@users.noreply.github.com>
2020-05-05 19:41:47 +0530

    Update IntroductiontoSets.py

--- Sets/IntroductiontoSets.py
+++ Sets/IntroductiontoSets.py
@@ -1,12 +1,2 @@
-'''
-Title     : Introduction to Sets
-Subdomain : Sets
-Domain    : Python
-Author    : Ahmedur Rahman Shovon
-Created   : 15 July 2016
-Problem   : https://www.hackerrank.com/challenges/py-introduction-to-sets/problem
-'''
-n = input()
-ar = map(int,input().split(' '))
-ar=set(ar)
-print(sum(ar) / len(ar))
+def average(array):
+    return sum(set(array))/len(set(array))
