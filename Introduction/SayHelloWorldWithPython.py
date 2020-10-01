0b2586987c416e43b69e9c2eff1085dfbe6db760
Durjay Dhali <47362849+Durjay-Dhali@users.noreply.github.com>
2020-10-01 17:21:00 +0600

    Update SayHelloWorldWithPython.py

--- Introduction/SayHelloWorldWithPython.py
+++ Introduction/SayHelloWorldWithPython.py
@@ -1,16 +1,2 @@
-'''
-Title     : Say Hello, World! With Python
-Subdomain : Introduction
-Domain    : Python
-Author    : Ahmedur Rahman Shovon
-Created   : 15 July 2016
-Problem   : https://www.hackerrank.com/challenges/py-hello-world/problem
-'''
-#This is a single line comment
-'''
-This is 
-a 
-multiline comment
-'''
-#write your code in next line. 
-print "Hello World!"
+if __name__ == '__main__':
+    print "Hello, World!"
