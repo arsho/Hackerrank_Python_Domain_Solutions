8319e5150f1c6c50c7a0cf891af2034a9626b375
Utkarsh Mishra <76392681+Utkarsh1504@users.noreply.github.com>
2021-03-14 13:46:21 +0530

    Update PythonIfElse.py

--- Introduction/PythonIfElse.py
+++ Introduction/PythonIfElse.py
@@ -11,9 +11,9 @@ Problem   : https://www.hackerrank.com/challenges/py-if-else/problem
 import sys
 
 n = int(input())
-if(n%2==1) or n in range (5,21):
+if n%2==1 or n in range (5,21):
     print("Weird")
 else:
     print("Not Weird")
 
-//I think this is simpler and easy from the previous.    
+    
