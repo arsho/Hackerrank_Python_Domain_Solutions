cc26d7df3882d71eda56a94cf8a006646f81cee5
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2021-03-15 16:06:27 +0530

    Update SymmetricDifference.py

--- Sets/SymmetricDifference.py
+++ Sets/SymmetricDifference.py
@@ -7,11 +7,11 @@ Created   : 15 July 2016
 Problem   : https://www.hackerrank.com/challenges/symmetric-difference/problem
 '''
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-m=int(raw_input())
-set_a_str_ar=raw_input().strip().split()
+m=int(input())
+set_a_str_ar=input().strip().split()
 set_a_ar=list(map(int,set_a_str_ar))
-n=int(raw_input())
-set_b_str_ar=raw_input().strip().split()
+n=int(input())
+set_b_str_ar=input().strip().split()
 set_b_ar=list(map(int,set_b_str_ar))
 
 set_a_set=set(set_a_ar)
@@ -22,4 +22,4 @@ res_set=set_a_dif_set.union(set_b_dif_set)
 res_ar=list(res_set)
 res_ar.sort()
 for i in res_ar:
-    print(i)
+    print (i)
