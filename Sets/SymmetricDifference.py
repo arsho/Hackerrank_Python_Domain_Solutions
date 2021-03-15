300328e63bdbe1f27a9e470c16f7eebd2cb7119f
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2021-03-15 23:40:02 +0530

    Updated Code as Requested
    Updated Date added in comments. Code changed as per suggestion.

--- Sets/SymmetricDifference.py
+++ Sets/SymmetricDifference.py
@@ -4,22 +4,14 @@ Subdomain : Sets
 Domain    : Python
 Author    : Ahmedur Rahman Shovon
 Created   : 15 July 2016
+Updated   : 15 March 2021
 Problem   : https://www.hackerrank.com/challenges/symmetric-difference/problem
 '''
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-m=int(input())
-set_a_str_ar=input().strip().split()
-set_a_ar=list(map(int,set_a_str_ar))
-n=int(input())
-set_b_str_ar=input().strip().split()
-set_b_ar=list(map(int,set_b_str_ar))
-
-set_a_set=set(set_a_ar)
-set_b_set=set(set_b_ar)
-set_a_dif_set=set_a_set.difference(set_b_set)
-set_b_dif_set=set_b_set.difference(set_a_set)
-res_set=set_a_dif_set.union(set_b_dif_set)
-res_ar=list(res_set)
-res_ar.sort()
-for i in res_ar:
-    print (i)
+m = int(input())
+set_a = set(map(int, input().split()))
+n = int(input())
+set_b = set(map(int, input().split()))
+set_a_diff = set_a.difference(set_b)
+set_b_diff = set_b.difference(set_a)
+for i in sorted(set_a_diff.union(set_b_diff)):
+    print(i)
