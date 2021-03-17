8a5ea922cf5075f088c9ad41e021536f3219cd9f
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2021-03-17 13:17:31 +0530

    Update FloorCeilandRint.py
    Alternate solution commented out. Original Solution retained as suggested.

--- Numpy/FloorCeilandRint.py
+++ Numpy/FloorCeilandRint.py
@@ -4,9 +4,16 @@ Subdomain : Numpy
 Domain    : Python
 Author    : Ahmedur Rahman Shovon
 Created   : 15 July 2016
+Updated   : 17 March 2021
 Problem   : https://www.hackerrank.com/challenges/floor-ceil-and-rint/problem
 '''
 import numpy as np
-np.set_printoptions(sign=' ')
-a = np.array(input().split(), float)
-print(*(f(a) for f in [np.floor, np.ceil, np.rint]), sep='\n')
+
+np.set_printoptions(legacy='1.13')
+A = np.array(input().split(), float)
+print(np.floor(A))
+print(np.ceil(A))
+print(np.rint(A))
+
+# Alternate solution
+# print(*(f(A) for f in [np.floor, np.ceil, np.rint]), sep='\n')
