207b7db010529632885a3e90f9a2ea3e3655bfb8
ssaumyaa7 <64359826+ssaumyaa7@users.noreply.github.com>
2021-03-23 19:42:44 +0530

    update for roundoff-value

--- Math/TriangleQuest2.py
+++ Math/TriangleQuest2.py
@@ -7,4 +7,4 @@ Created   : 15 July 2016
 Problem   : https://www.hackerrank.com/challenges/triangle-quest-2/problem
 '''
 for i in range(1,int(raw_input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
-    print(((10**i)/9)**2)
+    print(round((10**i)/9)**2)
