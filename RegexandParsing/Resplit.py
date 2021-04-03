6bf8fe62891837e48d31a1c24e4a21759ea9996a
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2021-04-03 12:08:42 +0530

    Update Resplit.py
    Working solution added. Previous solution failed all test cases.

--- RegexandParsing/Resplit.py
+++ RegexandParsing/Resplit.py
@@ -4,6 +4,11 @@ Subdomain : Regex and Parsing
 Domain    : Python
 Author    : Ahmedur Rahman Shovon
 Created   : 15 July 2016
+Updated   : 3 April 2021
 Problem   : https://www.hackerrank.com/challenges/re-split/problem
 '''
-import re;[print(i) for i in re.split("[,\.]?",input().strip()) if i]
+
+import re
+regex_pattern = r'[.,]+'
+
+print("\n".join(re.split(regex_pattern, input())))
