2f6b017761fd2a3506e809fb30e24445780ac15a
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2021-04-03 21:36:53 +0530

    Update DetectFloatingPointNumbers.py
    Regex Pattern updated as mentioned in review.

--- RegexandParsing/DetectFloatingPointNumbers.py
+++ RegexandParsing/DetectFloatingPointNumbers.py
@@ -9,6 +9,6 @@ Problem   : https://www.hackerrank.com/challenges/introduction-to-regex/problem
 
 from re import match, compile
 
-pattern = compile('^[-+]?[0-9]*\.[0-9]+$')
+pattern = compile('^[-+]?\d*\.\d+$')
 for _ in range(int(input())):
     print(bool(pattern.match(input())))
