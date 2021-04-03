c471ae065e4d38b87373c29b48d1c48253e5ef2d
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2021-04-03 11:42:14 +0530

    Update Input.py
    Working Solution Added.

--- BuiltIns/Input.py
+++ BuiltIns/Input.py
@@ -4,7 +4,15 @@ Subdomain : Built-Ins
 Domain    : Python
 Author    : Ahmedur Rahman Shovon
 Created   : 15 July 2016
+Updated   : 3 April 2021
 Problem   : https://www.hackerrank.com/challenges/input/problem
 '''
-x,k=list(map(int,raw_input().split()))
-print(input() == k)
+
+if __name__ == "__main__":
+    x, k = map(int, input().strip().split())
+    string = input().strip()
+    
+    if eval(string) == k:
+        print(True)
+    else:
+        print(False)
