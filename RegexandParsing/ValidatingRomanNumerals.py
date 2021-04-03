d3aa3fd57ff074dc857bad8f89f08f31f5ad8703
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2021-04-03 11:32:09 +0530

    Update ValidatingRomanNumerals.py
    Previous solution was out of date. Added valid, working solution.

--- RegexandParsing/ValidatingRomanNumerals.py
+++ RegexandParsing/ValidatingRomanNumerals.py
@@ -4,19 +4,11 @@ Subdomain : Regex and Parsing
 Domain    : Python
 Author    : Ahmedur Rahman Shovon
 Created   : 15 July 2016
+Updated   : 3 April 2021
 Problem   : https://www.hackerrank.com/challenges/validate-a-roman-number/problem
 '''
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+regex_pattern = r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"
+
 import re
-def my_func(s):
-    s = s.upper()
-    ##res=re.match(r'^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$',s)
-    res=re.search(r'^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$',s)
-    if(s=="MMMM"):
-        print "False"
-    else:
-        if res:
-            print "True"
-        else:
-            print "False"
-my_func(raw_input())
+print(str(bool(re.match(regex_pattern, input()))))
