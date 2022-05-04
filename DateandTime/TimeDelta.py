ee3e8a424adda5a6f92ad1690c872eef6e5ce540
Charan Sattu <71934330+charan-sattu@users.noreply.github.com>
2022-05-05 00:24:06 +0530

    Update TimeDelta.py
    updated code - appended with comment

--- DateandTime/TimeDelta.py
+++ DateandTime/TimeDelta.py
@@ -15,3 +15,40 @@ for t in range(cas):
     time_second1 = datetime.datetime.strptime(timestamp1,time_format)
     time_second2 = datetime.datetime.strptime(timestamp2,time_format)
     print(int(abs((time_second1-time_second2).total_seconds())))
+
+    
+    
+##    -------updated code----------
+
+import math
+import os
+import random
+import re
+import sys
+from datetime import datetime
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    timestamp1 = t1.strip()
+    timestamp2 = t2.strip()
+    time_format = "%a %d %b %Y %H:%M:%S %z"
+    time_second1 = datetime.strptime(timestamp1,time_format)
+    time_second2 = datetime.strptime(timestamp2,time_format)
+    print(int(abs((time_second1-time_second2).total_seconds())))
+    return (int(abs((time_second1-time_second2).total_seconds())))
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(str(int(delta)) + '\n')
+
+    fptr.close()
