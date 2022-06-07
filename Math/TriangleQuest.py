24b2d2d842b06c010c82a2967a7ddae400ff802f
bunai <80148425+vnaypd@users.noreply.github.com>
2022-06-07 22:20:10 +0530

    Update TriangleQuest.py
    it was printing floating value so need to use // for int value

--- Math/TriangleQuest.py
+++ Math/TriangleQuest.py
@@ -7,4 +7,4 @@ Created   : 15 July 2016
 Problem   : https://www.hackerrank.com/challenges/python-quest-1/problem
 '''
 for i in range(1,input()): #More than 2 lines will result in 0 score. Do not leave a blank line also
-    print i*((pow(10,i)-1)/(9))
+    print i*((10**i-1)//9)
