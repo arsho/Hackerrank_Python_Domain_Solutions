809c40442c3109c3c4e7e1724b18b50794d29060
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2023-02-08 21:08:30 -0600

    used f string

--- Collections/CollectionsOrderedDict.py
+++ Collections/CollectionsOrderedDict.py
@@ -4,6 +4,7 @@ Subdomain : Collections
 Domain    : Python
 Author    : Ahmedur Rahman Shovon
 Created   : 15 July 2016
+Updated   : 08 February 2023
 Problem   : https://www.hackerrank.com/challenges/py-collections-ordereddict/problem
 """
 
@@ -16,8 +17,9 @@ for _ in range(n):
     record_list = re.split(r"(\d+)$", input().strip())
     item_name = record_list[0]
     item_price = int(record_list[1])
-    item_od[item_name] = (
-        item_price if item_name not in item_od else item_od[item_name] + item_price
-    )
+    if item_name not in item_od:
+        item_od[item_name] = item_price
+    else:
+        item_od[item_name] = item_od[item_name] + item_price
 for i in item_od:
-    print(i + str(item_od[i]))
+    print(f"{i}{item_od[i]}")
