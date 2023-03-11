d92037ed8ef6c0bfc51cd9a4880b14a9e4ab00cf
Edward Storks <115942881+edwardstorks@users.noreply.github.com>
2023-03-11 20:28:46 +0530

    Update AlphabetRangoli.py

--- Strings/AlphabetRangoli.py
+++ Strings/AlphabetRangoli.py
@@ -7,35 +7,11 @@ Created   : 15 July 2016
 Problem   : https://www.hackerrank.com/challenges/alphabet-rangoli/problem
 """
 
-n = int(input().strip())
-w = (n - 1) * 2 + ((n * 2) - 1)
-# upper half
-for i in range(1, n):
-    number_of_letter = (i * 2) - 1
-    s = ""
-    letter_value = 97 + n - 1
-    for i in range(number_of_letter):
-        if i != 0:
-            s += "-"
-        s += chr(letter_value)
-        if i < (number_of_letter - 1) / 2:
-            letter_value = letter_value - 1
-        else:
-            letter_value = letter_value + 1
-    print(s.center(w, "-"))
-
-
-# bottom half
-for i in range(n, 0, -1):
-    number_of_letter = (i * 2) - 1
-    s = ""
-    letter_value = 97 + n - 1
-    for i in range(number_of_letter):
-        if i != 0:
-            s += "-"
-        s += chr(letter_value)
-        if i < (number_of_letter - 1) / 2:
-            letter_value = letter_value - 1
-        else:
-            letter_value = letter_value + 1
-    print(s.center(w, "-"))
+import string
+    design = string.ascii_lowercase
+    L = []
+    for i in range(n):
+        s = "-".join(design[i:n])
+        L.append((s[::-1]+s[1:]).center(4*n-3, "-"))
+        
+    print('\n'.join(L[:0:-1]+L))
