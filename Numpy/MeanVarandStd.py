0b1ced406c551cf79785dba0a9ea6a14b6b695f8
Dr. J. M. Ashfaque, MInstP, MAAT, AATQB <39216339+UKVeteran@users.noreply.github.com>
2023-06-24 05:48:37 +0100

    Update MeanVarandStd.py

--- Numpy/MeanVarandStd.py
+++ Numpy/MeanVarandStd.py
@@ -8,13 +8,28 @@ Problem   : https://www.hackerrank.com/challenges/np-mean-var-and-std/problem
 """
 
 import numpy
+nm = input()
+inm = nm.split( )
+inn = []
+for i in inm:
+    i = int(i)
+    inn.append(i)
 
-n, m = map(int, input().split())
-ar = []
-for _ in range(n):
-    tmp = list(map(int, input().split()))
-    ar.append(tmp)
-np_ar = numpy.array(ar)
-print(numpy.mean(np_ar, axis=1))
-print(numpy.var(np_ar, axis=0))
-print(numpy.std(np_ar, axis=None))
+arr = []
+
+for i in range(0, inn[0]):
+    a = input()
+    k = []
+    b = a.split(" ")
+
+    for i in b:
+        i = int(i)
+        k.append(i)
+    arr.append(k)
+
+arn = numpy.array(arr)
+print(numpy.mean(arn, axis=1))
+print(numpy.var(arn, axis=0))
+z = numpy.std(arn, axis=None)
+j = round(z, 11)
+print(j)
